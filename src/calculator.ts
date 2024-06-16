05f3adcb005a197503a17a8ccfeac5a53a18eeae
SurajWadje <suraj@litmusblox.io>
2024-06-16 07:05:12 +0530

    refactor: cleaned code

--- src/calculator.ts
+++ src/calculator.ts
@@ -5,15 +5,18 @@ export const add = (numbers : string)=>{
 
     if(numbers){
         const nonDigitRegex = /[^\d-]/g;
-        const filteredNumbers = numbers.replace(nonDigitRegex,' ')
-        console.log(filteredNumbers)
-        const numbersArray = filteredNumbers.split(' ').map(Number);
+        const delimiter = ' ';
+        const greatestAllowedNumber = 1000;
+
+        const filteredNumbers = numbers.replace(nonDigitRegex,delimiter)
+        const numbersArray = filteredNumbers.split(delimiter).map(Number);
         const negativeNumbers = numbersArray.filter(num=>num<0);
+
         if(negativeNumbers.length>0){
             throw new Error(`negative numbers not allowed ${negativeNumbers.join(',')}`)
         }
-       
-        sum = numbersArray.filter(num=>num<=1000).reduce((prev,current)=>prev+current);
+    
+        sum = numbersArray.filter(num=>num<=greatestAllowedNumber).reduce((prev,current)=>prev+current);
     }
 
     return sum;
