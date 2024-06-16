740701c6cee8295c23f75f5e2d416ddad314a36a
SurajWadje <suraj@litmusblox.io>
2024-06-16 07:13:34 +0530

    chore: updated jest config

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,9 @@ module.exports = {
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
+  testMatch: [
+    '**/src/**/*.test.ts',  // Adjust the glob pattern as per your folder structure
+  ],
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
